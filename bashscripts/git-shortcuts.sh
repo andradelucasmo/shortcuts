6581a432efc9327abbe65a0c1c656b3389467bde
L Andrade <andradelucasmo@trabalho.com>
2024-07-09 22:30:35 -0300

    multiple git configuration

--- bashscripts/git-shortcuts.sh
+++ bashscripts/git-shortcuts.sh
@@ -1,8 +1,65 @@
 git-start() {
-    echo -e "* Defining global user name"
-    git config --global user.name "Andrade"
-    echo -e "* Defining global user email"
-    git config --global user.email "andradelucasmo@gmail.com"
-    echo -e "* Git configuration info"
-    git config --list
+
+    # Setting personal informations
+    WORK_NAME="<your-work-name>"
+    WORK_EMAIL="<your-work-email>"
+
+    PERSONAL_NAME="<your-personal-name>"
+    PERSONAL_EMAIL="<your-personal-email>"
+
+    UNIVERSITY_NAME="<your-university-name>"
+    UNIVERSITY_EMAIL="<your-university-email>"
+
+    # Setting variables to the personal and university folders
+    PERSONAL_DIR="$HOME/lucas"
+    UNIVERSITY_DIR="$HOME/university"
+
+    # Setting variables to the configuration path
+    PERSONAL_CONFIG="$HOME/.gitconfig_lucas"
+    UNIVERSITY_CONFIG="$HOME/.gitconfig_university"
+
+    # If the directories do not exist, create them and show a success message
+    [ ! -d "$PERSONAL_DIR" ] && mkdir -p "$PERSONAL_DIR" && echo "* Created directory $PERSONAL_DIR"
+    [ ! -d "$UNIVERSITY_DIR" ] && mkdir -p "$UNIVERSITY_DIR" && echo "* Created directory $UNIVERSITY_DIR"
+
+    # Generate the code for the personal directory file
+    echo "* Configuring Git for personal directory"
+    cat > "$PERSONAL_CONFIG" <<EOL
+[user]
+    name = "$PERSONAL_NAME"
+    email = "$PERSONAL_EMAIL"
+EOL
+
+    echo "* Configuring Git for university directory"
+    cat > "$UNIVERSITY_CONFIG" <<EOL
+[user]
+    name = "$UNIVERSITY_NAME"
+    email = "$UNIVERSITY_EMAIL"
+EOL
+
+    # Update .gitconfig to import the new files
+    echo "* Updating global .gitconfig"
+    cat > "$HOME/.gitconfig" <<EOL
+[user]
+    name = "$WORK_NAME"
+    email = "$WORK_EMAIL"
+
+[includeIf "gitdir:$PERSONAL_DIR/"]
+    path = $PERSONAL_CONFIG
+
+[includeIf "gitdir:$UNIVERSITY_DIR/"]
+    path = $UNIVERSITY_CONFIG
+EOL
+
+    echo "* Git configuration completed successfully"
+
+    # Verify configuration in the personal directory
+    cd $PERSONAL_DIR
+    echo "* Verifying personal directory configuration"
+    git config --list | grep user.name && git config --list | grep user.email
+
+    # Verify configuration in the university directory
+    cd $UNIVERSITY_DIR
+    echo "* Verifying university directory configuration"
+    git config --list | grep user.name && git config --list | grep user.email
 }
